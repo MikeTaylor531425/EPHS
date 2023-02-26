352705e3881c137c3f71d81532c890dbf1dbca5e
MikeTaylor531425 <120058327+MikeTaylor531425@users.noreply.github.com>
2023-02-26 14:46:42 +0000

    Update action_page.php
    Signed-off-by: MikeTaylor531425 <120058327+MikeTaylor531425@users.noreply.github.com>

--- action_page.php
+++ action_page.php
@@ -1,8 +1,8 @@
 <?php
   // The global $_POST variable allows you to access the data sent with the POST method by name
   // To access the data sent with the GET method, you can use $_GET
-  $uname = htmlspecialchars($_POST['say']);
-  $password  = htmlspecialchars($_POST['to']);
+  $username = htmlspecialchars($_POST['username']);
+  $password  = htmlspecialchars($_POST['password']);
 
-  echo  $uname, ' ', $password;
+  echo  $username, ' ', $password;
 ?>
